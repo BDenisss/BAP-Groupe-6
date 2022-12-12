6310f9b6caf262c87082a63cfb05da7a394384bd
Denis Bucspun <bucspun.d@gmail.com>
2022-12-12 12:29:25 +0100

    :construction: In Progress

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@ import Button1 from "./components/Button1.vue"
 
 <template>
 
-  <div class="flex justify-center img bg-cover w-[100%] h-[100vh]">
-    <div class="flex justify-center items-center">
+<div class="img bg-cover h-[100vh]">
+  <div class="flex justify-center img bg-cover w-[100%] h-[100%]">
+    <div class="flex flex-col justify-center items-center gap-y-9">
+      <h1 class="text-white text-xl"> Déposez vos meubles à vendre sur notre site et laissez nous s'occuper du reste.</h1>
       <Button1 class="rounded-full" text="Catalogue"/>
     </div>
   </div>
+</div>
+
 
 
 </template>
@@ -16,6 +20,7 @@ import Button1 from "./components/Button1.vue"
 <style scoped>
 
 .img {
-  background-image: url("public/img/Vector 4.png");
+  background-image: url("public/img/bg.svg");
 }
+
 </style>
--- src/components/Button1.vue
+++ src/components/Button1.vue
@@ -4,7 +4,7 @@ defineProps(['text'])
 
 <template>
 
-  <div class="class= p-[6px] items-center bg-[#FFFFFF]/[.08] rounded-full drop-shadow-md	">
+  <div class=" anim p-[6px] items-center bg-[#FFFFFF]/[.08] rounded-full drop-shadow-md	">
       <button class="bg-[#FFFFFF] text-[#4B5D4C] font-normal py-[11px] px-[52px] rounded-full text-2xl">{{text}}</button>
   </div>
 </template>
