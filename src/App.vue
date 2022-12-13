ddd51b6577dd32754109a9318773933809a6a411
Denis Bucspun <bucspun.d@gmail.com>
2022-12-13 12:39:16 +0100

    :construction: In Progress

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import Button1 from "./components/Button1.vue"
 
 <template>
 
-<div class="img bg-cover h-full no-repeats">
+<div class="img bg-cover h-full max-sm:gradiant">
   <div class="flex justify-center w-[100%] h-[800px]">
     <div class="flex flex-col justify-center items-center gap-y-9">
-      <h1 class="text-white text-xl text-center"> Déposez vos meubles à vendre sur notre site et laissez nous s'occuper du reste.</h1>
+      <h1 class="text-white text-xl text-center px-6 max-sm:text-[17px]" id="title"> Déposez vos meubles à vendre sur notre site et laissez nous s'occuper du reste.</h1>
       <Button1 class="rounded-full" text="Catalogue"/>
     </div>
   </div>
@@ -18,8 +18,23 @@ import Button1 from "./components/Button1.vue"
 <div class="mt-[800px]">
 
   <div class="relative">
-    <img class="w-full absolute bottom-0" src="public/img/Vector%206.svg" alt="path2">
-    <img class="w-full absolute bottom-0" src="public/img/Vector%208.svg" alt="path2">
+    <img class="w-full absolute bottom-0 max-sm:hidden" src="public/img/Vector%206.svg" alt="path6">
+    <img class="w-full absolute bottom-0 max-sm:hidden" src="public/img/Vector%207.svg" alt="path7">
+    <img class="w-full absolute bottom-0 max-sm:hidden" src="public/img/Vector%209.svg" alt="path9">
+
+    <div class="flex justify-center gap-x-8 mb-[150px] max-md:mb-4 max-lg:mb-6 max-xl:mb-16 max-sm:flex-col max-sm:gap-y-8">
+      <div class="flex flex-col max-sm:gap-y-1">
+        <img class="h-[100px] z-10 max-md:h-[50px] max-sm:hidden" src="public/img/Group%2079.svg" alt="icone-arbre">
+        <img class="h-[100px] z-10 max-md:h-[50px] sm:hidden" src="public/img/black-tree.svg" alt="icone-arbre">
+        <p class="text-white text-xl font-light z-10 text-center max-md:text-[16px] max-sm:text-black">Une démarche 100 % éco-responsable</p>
+      </div>
+      <div class="flex flex-col max-sm:gap-y-1">
+        <img class="h-[100px] z-10 max-md:h-[50px] max-sm:hidden" src="public/img/Group%2078.svg" alt="icone-arbre">
+        <img class="h-[100px] z-10 max-md:h-[50px] sm:hidden" src="public/img/black-eco.svg" alt="icone-arbre">
+        <p class="text-white text-xl font-light z-10 text-center max-md:text-[16px] max-sm:text-black">Redonnez une vie à un ancien meuble</p>
+      </div>
+    </div>
+
     <div class="rectangle"/>
   </div>
 </div>
@@ -33,17 +48,19 @@ import Button1 from "./components/Button1.vue"
 
 <style scoped>
 
+.gradiant {
+  background-color: #4B5D4C;
+}
+
+
 .img {
   background-image:
       linear-gradient(rgba(131, 154, 126, 0.50), rgba(131, 154, 126, 0.50)),
       url("public/img/meubles.png");
-  background-size: cover;
   background-blend-mode: normal;
-
-}
-
-.curv-bg {
-  background-color: #4B5D4C;
+  width: 100%;
+  border-bottom: 10px solid black;
+  clip-path: path("M523.853 648.417C414.649 853.596 147.255 761.953 27.2087 690.484H-12V-178H1752.22L1784 77.8346L1752.22 325.449C1732.47 237.515 1652.1 432.984 1578.04 510.786C1507.85 611.656 1199.7 713.01 946.436 558.854C693.177 404.699 559.189 554.331 523.853 648.417Z");
 }
 
 .rectangle {
@@ -55,6 +72,11 @@ import Button1 from "./components/Button1.vue"
   border-image: linear-gradient(to right, rgba(255, 255, 255, 0.19) 49.43%, rgba(255, 255, 255, 0) 74.36%) 1;
 }
 
+@media screen and (max-width: 768px) {
+  .rectangle {
+    height: 30px;
+  }
+}
 
 
 </style>
--- src/components/Button1.vue
+++ src/components/Button1.vue
@@ -5,7 +5,7 @@ defineProps(['text'])
 <template>
 
   <div class=" anim p-[6px] items-center bg-[#FFFFFF]/[.08] rounded-full drop-shadow-md	">
-      <button class="bg-[#FFFFFF] text-[#4B5D4C] font-normal py-[11px] px-[52px] rounded-full text-2xl">{{text}}</button>
+      <button class="bg-[#FFFFFF] text-[#4B5D4C] font-normal py-[11px] px-[52px] rounded-full text-2xl max-sm:py-[8px] max-sm:px-[32px] max-sm:text-[20px]" id="btn">{{text}}</button>
   </div>
 </template>
 
