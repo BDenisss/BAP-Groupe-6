382b8a514e29ecd8c69e56466299a275a80231d8
Denis Bucspun <bucspun.d@gmail.com>
2022-12-16 15:37:25 +0100

    :art: improve code

--- src/main.js
+++ src/main.js
@@ -116,21 +116,6 @@ var swiper = new Swiper(".mySwiper", {
                 prevEl: ".swiper-button-prev",
               },
         },
-        200: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            slidesPerGroup: 1,
-            loop: true,
-            loopFillGroupWithBlank: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-              },
-              navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-              },
-        }
     }
 });
   
